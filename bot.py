2d4b0eeba7215ee23f695136419793bd91b91373
Jake Crowley <jakecrow81@gmail.com>
2023-01-31 18:33:00 -0600

    Added .prime sets to show, textually, numbers for all sets cached

--- bot.py
+++ bot.py
@@ -988,5 +988,56 @@ async def on_message(message):
         txncount = faucet()
         await message.channel.send(f"`Total faucet pulls : {txncount}`")
 
+    if message.content.lower() == '.prime sets':
+        ctx = await message.channel.send("`Processing, please be patient.`")
+        PD1cbtotalCached, PD1cb = PD1cbcall()
+        PD2cbtotalCached, PD2cb = PD2cbcall()
+        PD3cbtotalCached, PD3cb = PD3cbcall()
+        PD4cbtotalCached, PD4cb = PD4cbcall()
+        PD5cbtotalCached, PD5cb = PD5cbcall()
+        PD6cbtotalCached, PD6cb = PD6cbcall()
+        PS15cbtotalCached, PS15cb = PS15cbcall()
+        PD1setotalCached, PD1se = PD1secall()
+        PD2setotalCached, PD2se = PD2secall()
+        PD3setotalCached, PD3se = PD3secall()
+        PD4setotalCached, PD4se = PD4secall()
+        PD5setotalCached, PD5se = PD5secall()
+        PD6setotalCached, PD6se = PD6secall()
+        PS15setotalCached, PS15se = PS15secall()
+        PD2pltotalCached, PD2pl = PD2plcall()
+        PD3pltotalCached, PD3pl = PD3plcall()
+        PD5pltotalCached, PD5pl = PD5plcall()
+        PD6pltotalCached, PD6pl = PD6plcall()
+        PD1totalCached, PD1 = PD1call()
+        PD2totalCached, PD2 = PD2call()
+        PD3totalCached, PD3 = PD3call()
+        PD4totalCached, PD4 = PD4call()
+        PD5totalCached, PD5 = PD5call()
+        PD6totalCached, PD6 = PD6call()
+        PS15totalCached, PS15 = PS15call()
+        PD1arttotalCached, PD1art = PD1artcall()
+        PD2arttotalCached, PD2art = PD2artcall()
+        PD3arttotalCached, PD3art = PD3artcall()
+        PD4arttotalCached, PD4art = PD4artcall()
+        PD5arttotalCached, PD5art = PD5artcall()
+        PD6arttotalCached, PD6art = PD6artcall()
+        PS15arttotalCached, PS15art = PS15artcall()
+        FeTotal = PS15totalCached + PD1totalCached + PD2totalCached + PD3totalCached + PD4totalCached + PD5totalCached + PD6totalCached
+        SeTotal = PS15setotalCached + PD1setotalCached + PD2setotalCached + PD3setotalCached + PD4setotalCached + PD5setotalCached + PD6setotalCached
+        CbTotal = PS15cbtotalCached + PD1cbtotalCached + PD2cbtotalCached + PD3cbtotalCached + PD4cbtotalCached + PD5cbtotalCached + PD6cbtotalCached
+        PlTotal = PD2pltotalCached + PD3pltotalCached + PD5pltotalCached + PD6pltotalCached
+        AcTotal = PS15arttotalCached + PD1arttotalCached + PD2arttotalCached + PD3arttotalCached + PD4arttotalCached + PD5arttotalCached + PD6arttotalCached
+        overallTotal = FeTotal + SeTotal + CbTotal + PlTotal + AcTotal
+        await message.channel.send(f"```ansi\n\u001b[0;37m           PS15  |  PD1   |  PD2   |  PD3   |  PD4   |  PD5    |  PD6   |  Totals\
+        \n\u001b[0;34mFE\u001b[0;37m      | \u001b[0;33m {PS15totalCached}  \u001b[0;37m | \u001b[0;33m {PD1totalCached}  \u001b[0;37m | \u001b[0;33m {PD2totalCached}  \u001b[0;37m | \u001b[0;33m {PD3totalCached}  \u001b[0;37m | \u001b[0;33m {PD4totalCached}  \u001b[0;37m | \u001b[0;33m {PD5totalCached}  \u001b[0;37m  | \u001b[0;33m {PD6totalCached} \u001b[0;37m  | \u001b[0;33m {FeTotal:,} \
+        \n\u001b[0;34mSE\u001b[0;37m      | \u001b[0;33m {PS15setotalCached}   \u001b[0;37m | \u001b[0;33m {PD1setotalCached}   \u001b[0;37m | \u001b[0;33m {PD2setotalCached}   \u001b[0;37m | \u001b[0;33m {PD3setotalCached}   \u001b[0;37m | \u001b[0;33m {PD4setotalCached}  \u001b[0;37m | \u001b[0;33m {PD5setotalCached}  \u001b[0;37m  | \u001b[0;33m {PD6setotalCached} \u001b[0;37m   | \u001b[0;33m {SeTotal:,} \
+        \n\u001b[0;34mPL\u001b[0;37m      | \u001b[0;33m 0    \u001b[0;37m | \u001b[0;33m 0    \u001b[0;37m | \u001b[0;33m {PD2pltotalCached}  \u001b[0;37m | \u001b[0;33m {PD3pltotalCached}   \u001b[0;37m | \u001b[0;33m 0    \u001b[0;37m | \u001b[0;33m {PD5pltotalCached}  \u001b[0;37m  | \u001b[0;33m {PD6pltotalCached} \u001b[0;37m  | \u001b[0;33m {PlTotal:,} \
+        \n\u001b[0;34mCB\u001b[0;37m      | \u001b[0;33m {PS15cbtotalCached}   \u001b[0;37m | \u001b[0;33m {PD1cbtotalCached}   \u001b[0;37m | \u001b[0;33m {PD2cbtotalCached}   \u001b[0;37m | \u001b[0;33m {PD3cbtotalCached}   \u001b[0;37m | \u001b[0;33m {PD4cbtotalCached}   \u001b[0;37m | \u001b[0;33m {PD5cbtotalCached}   \u001b[0;37m  | \u001b[0;33m {PD6cbtotalCached} \u001b[0;37m   | \u001b[0;33m {CbTotal:,} \
+        \n\u001b[0;34mART\u001b[0;37m     | \u001b[0;33m {PS15arttotalCached}    \u001b[0;37m | \u001b[0;33m {PD1arttotalCached}    \u001b[0;37m | \u001b[0;33m {PD2arttotalCached}    \u001b[0;37m | \u001b[0;33m {PD3arttotalCached}    \u001b[0;37m | \u001b[0;33m {PD4arttotalCached}    \u001b[0;37m | \u001b[0;33m {PD5arttotalCached}    \u001b[0;37m  | \u001b[0;33m {PD6arttotalCached} \u001b[0;37m    | \u001b[0;33m {AcTotal:,} \
+        \n\u001b[0m-----------------------------------------------------------------\
+        \n\u001b[0;34mGrand total: \u001b[1;33m{overallTotal:,}\
+        ```")
+        await ctx.edit(content="**`Overview of all sets cached: (please note this data is intended as an estimate only)`**")
+
 
 client.run(TOKEN)
