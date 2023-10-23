f1212145ac70589e8e1681128a7e9799c00e2b40
Jake Crowley <jakecrow81@gmail.com>
2023-10-23 16:17:44 -0500

    -update echocall to exclude team test transactions

--- requestsCalls.py
+++ requestsCalls.py
@@ -9,14 +9,16 @@ load_dotenv()
 basescanApi = os.getenv('BASESCAN_API')
 chainbaseApi = os.getenv('CHAINBASE_API')
 
-
 #Echos block, get response from Basescan
 def echoCall():
-    response = requests.get(f'https://api.basescan.org/api?module=account&action=tokentx&contractAddress=0xfa980ced6895ac314e7de34ef1bfae90a5add21b&address=0xf507d0073039551907eb55bdc2c62c894641cfee&page=1&offset=10000&startblock=0&endblock=27025780&sort=asc&apikey={basescanApi}').json()
+    #build list of txn hashes to skip, this is manual because we can't know ahead of time which transactions need to go in here
+    txnToSkip = ['0x924bd51c1e43556b90041aee418c1c0e2365803eb860014bc86ff72bf64e666c', '0xc3af1ae473a2c3e1183cbf8f54c6fc5c6c1329987e539e052be8f4f970464fff', '0x636eb6d3dab6f54cd933679ac1a8687ea360b336212d3b99c803dbaddf6f5193', '0x15af130d05eed2af77f29aa1f3816bfc214bfa136f2a82fd6d24276e820fd281']
+
+    response = requests.get(f'https://api.basescan.org/api?module=account&action=tokentx&contractAddress=0xfa980ced6895ac314e7de34ef1bfae90a5add21b&address=0xf507d0073039551907eb55bdc2c62c894641cfee&page=1&offset=10000&sort=asc&apikey={basescanApi}').json()
     echoPrime = 0
     for i in range(len(response['result'])):
-        if response['result'][i]['tokenName'] == 'Prime':
-            echoPrime = echoPrime + int(response['result'][i]['value'])
+        if response['result'][i]['tokenName'] == 'Prime' and response['result'][i]['to'] == '0xf507d0073039551907eb55bdc2c62c894641cfee' and response['result'][i]['hash'] not in txnToSkip:
+            echoPrime = echoPrime + float(response['result'][i]['value'])
     finalPrime = round(echoPrime / 1000000000000000000, 3)
     return int(finalPrime)
 
